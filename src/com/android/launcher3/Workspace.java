97c40dbcf2a57802bb75991a384d2b7955da2f61
spezi77 <spezi7713@gmx.net>
2019-02-15 18:52:53 +0100

    Switch to ActionUtils.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -57,7 +57,7 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
-import com.android.internal.util.pixeldust.PixeldustUtils;
+import com.android.internal.util.pixeldust.ActionUtils;
 
 import com.android.launcher3.Launcher.LauncherOverlay;
 import com.android.launcher3.LauncherAppWidgetHost.ProviderChangedListener;
@@ -329,11 +329,11 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
                 break;
             // Sleep
             case 1:
-                PixeldustUtils.switchScreenOff(getContext());
+                ActionUtils.switchScreenOff(getContext());
                 break;
             // Flashlight
             case 2:
-                PixeldustUtils.toggleCameraFlash();
+                ActionUtils.toggleCameraFlash();
                 break;
         }
     }
